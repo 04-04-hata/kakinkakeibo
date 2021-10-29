af00380159dd77ef8d801147dc7d2df887878aa8
04-04-hata <zhenlinaitian@gmail.com>
2021-10-29 08:01:03 +0000

    jsファイルのletをvarにした

--- app/assets/javascripts/gacha_kakeibo_form.js
+++ app/assets/javascripts/gacha_kakeibo_form.js
@@ -1,7 +1,7 @@
 function buttonClick(){
-  let gacha_kakeibo_is_result_status_false = document.getElementById("gacha_kakeibo_is_result_status_false");
-  let winning_count = document.getElementById("winning_count");
-  let gacha_kakeibo_winning_count = document.getElementById("gacha_kakeibo_winning_count");
+  var gacha_kakeibo_is_result_status_false = document.getElementById("gacha_kakeibo_is_result_status_false");
+  var winning_count = document.getElementById("winning_count");
+  var gacha_kakeibo_winning_count = document.getElementById("gacha_kakeibo_winning_count");
   if(gacha_kakeibo_is_result_status_false.checked){
     winning_count.style.display = "none";
     gacha_kakeibo_winning_count.value = "";
